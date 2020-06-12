e3bbb8c5aec82c552fbf87edc321ac388ba53cf7
Dean Jeffery <itsdeanrj@gmail.com>
2020-06-12 10:12:38 +0100

    log in working
    begining working on gui with tkinter

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 import datetime
 import currentaccount
 import savingsaccount
+import csv
+
 
 # Define global variables
 x = datetime.datetime.now()
@@ -16,36 +18,48 @@ x = datetime.datetime.now()
 #####
 #####
 
-logged = False
+def read_data():
+    data = []
+    # open and read data from csv file
+    with open('users.csv') as csv_file:
+        read_csv = csv.reader(csv_file, delimiter=',')
+        # read and store data
+        for row in read_csv:
+            data.append(row)
+    # return list of elements
+    return data
 
-# Username, Password
-accounts = [
-    ["admin", "1234"]
-]
 
-# Store logged in users details
-loggedUser = []
+def check_login(username, password, users):
+    # cycle through rows to find username and password match
+    for row in users:
+        # check for match
+        if username == row[1] and password == row[2]:
+            # return true if match is found
+            return row
+    # return false if no match is found
+    return False
 
-# Take inputs for the user to login to the system
-username = input("Enter your username: ")
-password = input("Enter your password: ")
 
-# Search through the rows in the 2D lists, to find a matching username and password in the same row
-for row in accounts:
-    if row[0] == username and row[1] == password:
-        print("Details correct - logged in!")
-        logged = True
-
+def login(username, password, user_data):
+    # call check_login function to validate
+    check = check_login(username, password, user_data)
+    # if check_login returns False:
+    if not check:
+        print("Invalid Login!")
+    # if check_login does not return False:
     else:
-        print("Details incorrect, try again")
+        print("Welcome,", check[3])
 
-        # We require this section to create a single list, rather than a 2D list
-        for column in row:
-            loggedUser.append(column)
-        break
 
-# Menu system - this does not activate until the user is confirmed to have matching credentials
-while logged:
+def question():
+    # request username and password
+    username = input("Please enter Username: ")
+    password = input("Please enter Password: ")
+    # call login function with user input + read_data() return
+    login(username, password, read_data())
+
+question()
 
 # END 0F LOGIN FORM
 
@@ -53,33 +67,28 @@ while logged:
 # START OF BANKING APP
 
 
-    print("\n\n\nTHIS IS A BANKING APP PROTOTYPE")
+print("\n\n\nTHIS IS A BANKING APP PROTOTYPE")
 
 
 
-    def account():
-        print("\n\n\nWould you like to make a changes to your current account or savings account? \n curr=Current save=Savings q=QUIT\n")
-        transact = str(input())
-        transact = transact.lower()
-        startup(transact)
+def account():
+    print("\n\n\nWould you like to make a changes to your current account or savings account? \n curr=Current save=Savings q=QUIT\n")
+    transact = str(input())
+    transact = transact.lower()
+    startup(transact)
 
-    def startup(confirm):
-        run = True
-        while run:
-            if confirm == "curr" or confirm == "current":
-                currentaccount.prompt()
-            elif confirm == "save" or confirm == "savings":
-                savingsaccount.prompt()
-            elif confirm == "q" or confirm == "quit":
-                print("Exiting...")
-                sys.exit()
-            else:
-                print("Input invalid")
-                account()
-
-
-    def main():
-        account()
-        
-
-    main()
+def startup(confirm):
+    run = True
+    while run:
+        if confirm == "curr" or confirm == "current":
+            currentaccount.prompt()
+        elif confirm == "save" or confirm == "savings":
+            savingsaccount.prompt()
+        elif confirm == "q" or confirm == "quit":
+            print("Exiting...")
+            sys.exit()
+        else:
+            print("Input invalid")
+            account()
+    
+account()
